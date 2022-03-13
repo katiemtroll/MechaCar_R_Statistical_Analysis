edb45ff1bed9f81567265a907964d8521620e090
Katie Troll <katiemtroll@gmail.com>
2022-03-13 15:55:00 -0500

    added code for deliverable 3

--- MechaCarChallenge.RScript.R
+++ MechaCarChallenge.RScript.R
@@ -13,6 +13,7 @@ mecha_mpg_lm = lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground
 summary(mecha_mpg_lm)
 # returned p-value of 5.35e-11 and r-squared of 0.7149, with adjusted r-squared of 0.6825.
 
+
 # Deliverable 2: Create Visualizations for the Trip Analysis
 #1. Import and read in the Suspension_Coil.csv file as a table
 mecha_suspension <- read.csv(file='Resources/Suspension_Coil.csv', check.names = F, stringsAsFactors = F)
@@ -23,4 +24,17 @@ total_summary <- mecha_suspension %>% summarize(Mean = mean(PSI), Median = media
 #3. Create a lot_summary df using group_by and summarize to group by manufacturing lot on PSI column.
 lot_summary <- mecha_suspension %>% group_by(Manufacturing_Lot) %>% summarize(No_of_Coils = n(), Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI), .groups='keep')
 
+
+# Deliverable 3: T-Tests on Suspension Coils
+#1. Determine if all lots are statistically different from the population mean of 1500 psi
+allt = t.test(mecha_suspension$PSI, mu=1500)
+
+#2. Use t.test() function 3 more times with subset() to determine if psi for each manufacturing lot is statistically different from the pop. mean of 1500 psi
+lot1 <- subset(mecha_suspension, Manufacturing_Lot=="Lot1")
+lot2 <- subset(mecha_suspension, Manufacturing_Lot=="Lot2")
+lot3 <- subset(mecha_suspension, Manufacturing_Lot=="Lot3")
+
+lot1_t = t.test(lot1$PSI,mu=1500)
+lot2_t = t.test(lot2$PSI,mu=1500)
+lot3_t = t.test(lot3$PSI,mu=1500)
 
